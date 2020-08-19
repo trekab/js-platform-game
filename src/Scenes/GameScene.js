a1deb212688b10e6d4e97f23e54e96ad8cf08660
Treasure Kabareebe <treasurecalm@gmail.com>
2020-08-20 00:49:14 +0300

    Add a scrolling parallax background

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -6,6 +6,9 @@ let gameOptions = {
   // platform speed range, in pixels per second
   platformSpeedRange: [300, 300],
  
+  // mountain speed, in pixels per second
+  mountainSpeed: 80,
+
   // spawn range, how far should be the rightmost platform from the right edge
   // before next platform spawns, in pixels
   spawnRange: [80, 300],
@@ -57,12 +60,18 @@ export default class GameScene extends Phaser.Scene {
       frameWidth: 20,
       frameHeight: 20
     });
+
+    // mountains are a sprite sheet made by 512x512 pixels
+    this.load.spritesheet("mountain", "assets/mountain.png", {
+      frameWidth: 512,
+      frameHeight: 512
+    });
   }
 
   create() {
 
-    // keeping track of added platforms
-    this.addedPlatforms = 0;
+    // group with all active mountains.
+    this.mountainGroup = this.add.group();
 
     // group with all active platforms.
     this.platformGroup = this.add.group({
@@ -100,15 +109,22 @@ export default class GameScene extends Phaser.Scene {
       }
     });
 
+    // adding a mountain
+    this.addMountains()
+
+    // keeping track of added platforms
+    this.addedPlatforms = 0;
+
     // number of consecutive jumps made by the player
     this.playerJumps = 0;
 
-    /// adding a platform to the game, the arguments are platform width, x position and y position
+    // adding a platform to the game, the arguments are platform width, x position and y position
     this.addPlatform(game.config.width, game.config.width / 2, game.config.height * gameOptions.platformVerticalLimit[1]);
 
     // adding the player;
     this.player = this.physics.add.sprite(gameOptions.playerStartPosition, game.config.height * 0.7, "player");
     this.player.setGravityY(gameOptions.playerGravity);
+    this.player.setDepth(2);
 
     // setting collisions between the player and the platform group
     this.physics.add.collider(this.player, this.platformGroup, function(){
@@ -171,6 +187,31 @@ export default class GameScene extends Phaser.Scene {
     this.input.on("pointerdown", this.jump, this);    
   }
 
+  // adding mountains
+  addMountains(){
+    let rightmostMountain = this.getRightmostMountain();
+    if(rightmostMountain < game.config.width * 2){
+        let mountain = this.physics.add.sprite(rightmostMountain + Phaser.Math.Between(100, 350), game.config.height + Phaser.Math.Between(0, 100), "mountain");
+        mountain.setOrigin(0.5, 1);
+        mountain.body.setVelocityX(gameOptions.mountainSpeed * -1)
+        this.mountainGroup.add(mountain);
+        if(Phaser.Math.Between(0, 1)){
+            mountain.setDepth(1);
+        }
+        mountain.setFrame(Phaser.Math.Between(0, 3))
+        this.addMountains()
+    }
+  }
+
+  // getting rightmost mountain x position
+  getRightmostMountain(){
+    let rightmostMountain = -200;
+    this.mountainGroup.getChildren().forEach(function(mountain){
+        rightmostMountain = Math.max(rightmostMountain, mountain.x);
+    })
+    return rightmostMountain;
+  }
+
   // the core of the script: platform are added from the pool or created on the fly
   addPlatform(platformWidth, posX, posY){
     this.addedPlatforms ++;
@@ -191,6 +232,7 @@ export default class GameScene extends Phaser.Scene {
         this.physics.add.existing(platform);
         platform.body.setImmovable(true);
         platform.body.setVelocityX(Phaser.Math.Between(gameOptions.platformSpeedRange[0], gameOptions.platformSpeedRange[1]) * -1);
+        platform.setDepth(2);
         this.platformGroup.add(platform);
     }
     this.nextPlatformDistance = Phaser.Math.Between(gameOptions.spawnRange[0], gameOptions.spawnRange[1]);
@@ -212,6 +254,7 @@ export default class GameScene extends Phaser.Scene {
                 coin.setImmovable(true);
                 coin.setVelocityX(platform.body.velocity.x);
                 coin.anims.play("rotate");
+                coin.setDepth(2);
                 this.coinGroup.add(coin);
             }
         }
@@ -221,14 +264,14 @@ export default class GameScene extends Phaser.Scene {
   // the player jumps when on the ground, or once in the air as long as there are jumps left and the first jump was on the ground
   jump(){
     if(this.player.body.touching.down || (this.playerJumps > 0 && this.playerJumps < gameOptions.jumps)){
-        if(this.player.body.touching.down){
-            this.playerJumps = 0;
-        }
-        this.player.setVelocityY(gameOptions.jumpForce * -1);
-        this.playerJumps ++;
+      if(this.player.body.touching.down){
+          this.playerJumps = 0;
+      }
+      this.player.setVelocityY(gameOptions.jumpForce * -1);
+      this.playerJumps ++;
 
-        // stops animation
-        this.player.anims.stop();
+      // stops animation
+      this.player.anims.stop();
     }
   }
 
@@ -259,6 +302,19 @@ export default class GameScene extends Phaser.Scene {
         if(coin.x < - coin.displayWidth / 2){
             this.coinGroup.killAndHide(coin);
             this.coinGroup.remove(coin);
+        }
+    }, this);
+
+    // recycling mountains
+    this.mountainGroup.getChildren().forEach(function(mountain){
+        if(mountain.x < - mountain.displayWidth){
+            let rightmostMountain = this.getRightmostMountain();
+            mountain.x = rightmostMountain + Phaser.Math.Between(100, 350);
+            mountain.y = game.config.height + Phaser.Math.Between(0, 100);
+            mountain.setFrame(Phaser.Math.Between(0, 3))
+            if(Phaser.Math.Between(0, 1)){
+                mountain.setDepth(1);
+            }
         }
     }, this);
 
